6dd66be33788fb18ce694eba0b015a916d7f9b0f
Mi <krashmisha@gmail.com>
2024-02-11 20:38:39 +0300

    rewrote basis for something new

--- drivers/mmc/host/virtio-mmc.c
+++ drivers/mmc/host/virtio-mmc.c
@@ -1,61 +1,18 @@
-#include "linux/blkdev.h"
-#include "linux/device.h"
-#include "linux/kstrtox.h"
-#include "linux/mod_devicetable.h"
-#include "linux/printk.h"
-#include "linux/scatterlist.h"
-#include "linux/slab.h"
-#include "linux/sysfs.h"
-#include "linux/virtio.h"
-#include "linux/virtio_config.h"
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/pci.h>
+#include "virtio-mmc.h"
 
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("VirtIO MMC driver");
-MODULE_AUTHOR("mi");
-
-#define VIRTIO_MMC_DEV_ID 42
-
-static struct virtio_device_id id_table[] = {
-    { 
-        .device = VIRTIO_MMC_DEV_ID,
-        .vendor = VIRTIO_DEV_ANY_ID,
-    },
-    { 0 },
-};
-
-struct virtmmc_info {
-    struct virtqueue *vq;
+struct virtmmc_data {
+	struct virtio_device *vdev;
+	struct mmc_host *mmc;
+	struct virtqueue *vq;
+	struct scatterlist sg;
 };
 
 static int virtio_mmc_probe(struct virtio_device *vdev) {
-    // Add dummy /dev/mmcblk0
-    printk(KERN_INFO "virtio_mmc_probe\n");
-    
-    int err = device_register(&vdev->dev);
-    if(err) {
-        printk(KERN_ERR "[virtio mmc] device_register failed\n");
-    }
+	printk(KERN_INFO "virtio_mmc_probe\n");
 
-    return 0;
+	return 0;
 }
 
 static void virtio_mmc_remove(struct virtio_device *vdev) {
-    printk(KERN_INFO "virtio_mmc_remove\n");
-    device_unregister(&vdev->dev);
+	printk(KERN_INFO "virtio_mmc_remove\n");
 }
-
-static struct virtio_driver virtio_mmc_driver = {
-    .driver = {
-        .name = "virtio_mmc",
-        .owner = THIS_MODULE,
-    },
-    .id_table = id_table,
-    .probe = virtio_mmc_probe,
-    .remove = virtio_mmc_remove,
-};
-
-module_virtio_driver(virtio_mmc_driver);
-MODULE_DEVICE_TABLE(virtio, id_table);
---
+++ drivers/mmc/host/virtio-mmc.h
@@ -0,0 +1,35 @@
+#ifndef _VIRTIO_MMC_H
+#define _VIRTIO_MMC_H
+
+#include <linux/virtio.h>
+#include <linux/virtio_ids.h>
+
+#define VIRTIO_MMC_DEV_ID 42
+
+static int virtio_mmc_probe(struct virtio_device *vdev);
+
+static void virtio_mmc_remove(struct virtio_device *vdev);
+
+static const struct virtio_device_id id_table[] = {
+	{ VIRTIO_ID_MMC, VIRTIO_MMC_DEV_ID },
+	{ 0 },
+};
+
+static struct virtio_driver virtio_mmc_driver = {
+    .driver = {
+        .name = KBUILD_MODNAME,
+        .owner = THIS_MODULE,
+    },
+    .id_table = id_table,
+    .probe = virtio_mmc_probe,
+    .remove = virtio_mmc_remove,
+};
+
+module_virtio_driver(virtio_mmc_driver);
+MODULE_DEVICE_TABLE(virtio, id_table);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("VirtIO MMC driver");
+MODULE_AUTHOR("mi");
+
+#endif
