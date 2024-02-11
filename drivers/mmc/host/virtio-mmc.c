4b5eb175b67cf29d61b91df168b4bb651bae90a5
Mi <krashmisha@gmail.com>
2024-02-11 23:27:15 +0300

    Add file_operations structure and implement open and release functions

--- drivers/mmc/host/virtio-mmc.c
+++ drivers/mmc/host/virtio-mmc.c
@@ -1,5 +1,6 @@
 #include "virtio-mmc.h"
 #include "linux/device/class.h"
+#include "linux/kobject.h"
 #include "linux/slab.h"
 #include <linux/fs.h>
 #include <linux/device.h>
@@ -9,23 +10,22 @@ struct virtmmc_data {
 	struct mmc_host *mmc;
 	struct virtqueue *vq;
 	struct scatterlist sg;
-	struct class* class;
 };
 
-// static int virtio_mmc_open(struct inode *inode, struct file *filp) {
-//     // Open implementation
-//     return 0;
-// }
+static int virtio_mmc_open(struct inode *inode, struct file *filp) {
+    // Open implementation
+    return 0;
+}
 
-// static int virtio_mmc_release(struct inode *inode, struct file *filp) {
-//     // Release implementation
-//     return 0;
-// }
+static int virtio_mmc_release(struct inode *inode, struct file *filp) {
+    // Release implementation
+    return 0;
+}
 
-// static struct file_operations virtio_mmc_fops = {
-//     .open = virtio_mmc_open,
-//     .release = virtio_mmc_release,
-// };
+static struct file_operations virtio_mmc_fops = {
+    .open = virtio_mmc_open,
+    .release = virtio_mmc_release,
+};
 
 
 static int virtio_mmc_probe(struct virtio_device *vdev) {
@@ -39,30 +39,29 @@ static int virtio_mmc_probe(struct virtio_device *vdev) {
 		printk(KERN_ERR "Failed to allocate memory for virtmmc_data\n");
 		return -ENOMEM;
 	}
+	vdev->priv = data;
 	printk(KERN_INFO "virtio_mmc_probe: data allocated\n");
 
-	vdev->priv = data;
-
-    data->class = class_create("virtmmc");
-    if (IS_ERR(data->class)) {
-        printk(KERN_ERR "Failed to create virtmmc class\n");
-        err = PTR_ERR(data->class);
+	err = register_chrdev(100, "virtio-mmc", &virtio_mmc_fops);
+	if (err < 0) {
+		printk(KERN_ERR "Failed to register char device\n");
 		goto free_data;
-    }
-	printk(KERN_INFO "virtio_mmc_probe: class created\n");
+	}
+	printk(KERN_INFO "virtio_mmc_probe: char device registered\n");
 
-    // Create the device node
-    if (!device_create(data->class, NULL, MKDEV(0, 0), NULL, "virtmmc")) {
-        printk(KERN_ERR "Failed to create virtmmc device\n");
-        err = -EFAULT;
-		goto destroy_class;
-    }
-	printk(KERN_INFO "virtio_mmc_probe: device created\n");
+	err = device_add(&vdev->dev);
+	if (err) {
+		printk(KERN_ERR "Failed to add device\n");
+		goto unregister_chrdev;
+	}
+	printk(KERN_INFO "virtio_mmc_probe: device added\n");
 
 	return 0;
 
-destroy_class:
-	class_destroy(data->class);
+remove_device:
+	device_del(&vdev->dev);
+unregister_chrdev:
+	unregister_chrdev(100, "virtio-mmc");
 free_data:
 	kfree(data);
 
@@ -74,8 +73,9 @@ static void virtio_mmc_remove(struct virtio_device *vdev) {
 
 	struct virtmmc_data *data = vdev->priv;
 
-	device_destroy(data->class, MKDEV(0, 0));
-	class_destroy(data->class);
+	device_del(&vdev->dev);
+
+	unregister_chrdev(100, "virtio-mmc");
 
 	kfree(data);
 
