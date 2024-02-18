4f19ad8ab5ff61ed873296728f06d41526d3be0e
Mi <krashmisha@gmail.com>
2024-02-18 14:33:42 +0300

    Add char device registration and creation

--- drivers/mmc/host/virtio-mmc.c
+++ drivers/mmc/host/virtio-mmc.c
@@ -1,7 +1,10 @@
 #include "virtio-mmc.h"
+#include "linux/cdev.h"
 #include "linux/device/class.h"
+#include "linux/kdev_t.h"
 #include "linux/kobject.h"
 #include "linux/slab.h"
+#include "linux/types.h"
 #include <linux/fs.h>
 #include <linux/device.h>
 
@@ -10,6 +13,10 @@ struct virtmmc_data {
 	struct mmc_host *mmc;
 	struct virtqueue *vq;
 	struct scatterlist sg;
+	dev_t *devt;
+	struct cdev *cdev;
+	struct device *device;
+	struct class *chardev_class;
 };
 
 static int virtio_mmc_open(struct inode *inode, struct file *filp) {
@@ -23,6 +30,7 @@ static int virtio_mmc_release(struct inode *inode, struct file *filp) {
 }
 
 static struct file_operations virtio_mmc_fops = {
+	.owner = THIS_MODULE,
     .open = virtio_mmc_open,
     .release = virtio_mmc_release,
 };
@@ -42,26 +50,50 @@ static int virtio_mmc_probe(struct virtio_device *vdev) {
 	vdev->priv = data;
 	printk(KERN_INFO "virtio_mmc_probe: data allocated\n");
 
-	err = register_chrdev(100, "virtio-mmc", &virtio_mmc_fops);
-	if (err < 0) {
-		printk(KERN_ERR "Failed to register char device\n");
+	int first_minor = 0;
+	unsigned minor_count = 1;
+	const char *dev_name = "mmcblk";
+	err = alloc_chrdev_region(data->devt, first_minor, minor_count, dev_name);
+	if (err) {
+		printk(KERN_ERR "Failed to allocate char device region\n");
 		goto free_data;
 	}
-	printk(KERN_INFO "virtio_mmc_probe: char device registered\n");
 
-	err = device_add(&vdev->dev);
-	if (err) {
-		printk(KERN_ERR "Failed to add device\n");
-		goto unregister_chrdev;
+	data->chardev_class = class_create(dev_name);
+	if (IS_ERR(data->chardev_class)) {
+		printk(KERN_ERR "Failed to create class\n");
+		err = PTR_ERR(data->chardev_class);
+		goto free_chrdev_region;
 	}
-	printk(KERN_INFO "virtio_mmc_probe: device added\n");
+
+	cdev_init(data->cdev, &virtio_mmc_fops);
+	data->cdev->owner = THIS_MODULE;
+
+	int dev_major = MAJOR(*data->devt);
+	err = cdev_add(data->cdev, MKDEV(dev_major, first_minor), minor_count);
+	if (err) {
+		printk(KERN_ERR "Failed to add cdev\n");
+		goto free_cdev;
+	}
+
+	data->device = device_create(data->chardev_class, NULL, MKDEV(dev_major, first_minor), NULL, dev_name);
+	if (IS_ERR(data->device)) {
+		printk(KERN_ERR "Failed to create device\n");
+		err = PTR_ERR(data->device);
+		goto free_chardev_class;
+	}
 
 	return 0;
 
-remove_device:
-	device_del(&vdev->dev);
-unregister_chrdev:
-	unregister_chrdev(100, "virtio-mmc");
+free_cdev:
+	cdev_del(data->cdev);
+
+free_chardev_class:
+	class_destroy(data->chardev_class);
+
+free_chrdev_region:
+	unregister_chrdev_region(*data->devt, minor_count);
+
 free_data:
 	kfree(data);
 
@@ -73,11 +105,9 @@ static void virtio_mmc_remove(struct virtio_device *vdev) {
 
 	struct virtmmc_data *data = vdev->priv;
 
-	device_del(&vdev->dev);
-
-	unregister_chrdev(100, "virtio-mmc");
-
+	device_destroy(data->chardev_class, *data->devt);
+	class_destroy(data->chardev_class);
+	cdev_del(data->cdev);
+	unregister_chrdev_region(*data->devt, 1);
 	kfree(data);
-
-	vdev->priv = NULL;
 }
