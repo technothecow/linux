b6c26b7567347be30cf3b8325423210926a6d12a
Mi <krashmisha@gmail.com>
2024-03-30 20:14:01 +0300

    More debug messages

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -140,6 +140,7 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 {
 	struct mmc_command *cmd = mrq->cmd;
 	int err = cmd->error;
+	printk(KERN_INFO "mmc_request_done 1, err = %d", err);
 
 	/* Flag re-tuning needed on CRC errors */
 	if (!mmc_op_tuning(cmd->opcode) &&
@@ -149,15 +150,23 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 	    (mrq->stop && mrq->stop->error == -EILSEQ)))
 		mmc_retune_needed(host);
 
+	printk(KERN_INFO "mmc_request_done 2");
+
 	if (err && cmd->retries && mmc_host_is_spi(host)) {
 		if (cmd->resp[0] & R1_SPI_ILLEGAL_COMMAND)
 			cmd->retries = 0;
 	}
 
+	printk(KERN_INFO "mmc_request_done 3");
+
 	if (host->ongoing_mrq == mrq)
 		host->ongoing_mrq = NULL;
 
+	printk(KERN_INFO "mmc_request_done 4");
+
 	mmc_complete_cmd(mrq);
+
+	printk(KERN_INFO "mmc_request_done 5");
 
 	trace_mmc_request_done(host, mrq);
 
@@ -170,12 +179,16 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 	 * - The card was removed (...so just complete everything no matter
 	 *   if there are errors or retries)
 	 */
+	printk(KERN_INFO "mmc_request_done 6");
 	if (!err || !cmd->retries || mmc_card_removed(host->card)) {
+		printk(KERN_INFO "mmc_request_done 6.1");
 		mmc_should_fail_request(host, mrq);
 
+		printk(KERN_INFO "mmc_request_done 6.2");
 		if (!host->ongoing_mrq)
 			led_trigger_event(host->led, LED_OFF);
 
+		printk(KERN_INFO "mmc_request_done 6.3");
 		if (mrq->sbc) {
 			pr_debug("%s: req done <CMD%u>: %d: %08x %08x %08x %08x\n",
 				mmc_hostname(host), mrq->sbc->opcode,
@@ -184,16 +197,19 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 				mrq->sbc->resp[2], mrq->sbc->resp[3]);
 		}
 
+		printk(KERN_INFO "mmc_request_done 6.4");
 		pr_debug("%s: req done (CMD%u): %d: %08x %08x %08x %08x\n",
 			mmc_hostname(host), cmd->opcode, err,
 			cmd->resp[0], cmd->resp[1],
 			cmd->resp[2], cmd->resp[3]);
+		printk(KERN_INFO "mmc_request_done 6.5");
 
 		if (mrq->data) {
 			pr_debug("%s:     %d bytes transferred: %d\n",
 				mmc_hostname(host),
 				mrq->data->bytes_xfered, mrq->data->error);
 		}
+		printk(KERN_INFO "mmc_request_done 6.6");
 
 		if (mrq->stop) {
 			pr_debug("%s:     (CMD%u): %d: %08x %08x %08x %08x\n",
@@ -202,14 +218,17 @@ void mmc_request_done(struct mmc_host *host, struct mmc_request *mrq)
 				mrq->stop->resp[0], mrq->stop->resp[1],
 				mrq->stop->resp[2], mrq->stop->resp[3]);
 		}
+		printk(KERN_INFO "mmc_request_done 6.7");
 	}
 	/*
 	 * Request starter must handle retries - see
 	 * mmc_wait_for_req_done().
 	 */
 
+	printk(KERN_INFO "mmc_request_done 7");
 	if (mrq->done)
 		mrq->done(mrq);
+	printk(KERN_INFO "mmc_request_done 8");
 }
 
 EXPORT_SYMBOL(mmc_request_done);
