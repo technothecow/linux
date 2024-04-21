e5bedce245266e3d33eb8becd78e8252085fc5cd
Mi <krashmisha@gmail.com>
2024-04-21 21:56:40 +0300

    Add MMC_CAP_DONE_COMPLETE flag to host capabilities

--- drivers/mmc/host/virtio-mmc.c
+++ drivers/mmc/host/virtio-mmc.c
@@ -188,7 +188,7 @@ static int create_host(struct virtio_device *vdev)
 	host->f_min = 300000;
 	host->f_max = 500000;
 	host->ocr_avail = MMC_VDD_32_33 | MMC_VDD_33_34;
-	host->caps = MMC_CAP_SD_HIGHSPEED;
+	host->caps = MMC_CAP_SD_HIGHSPEED | MMC_CAP_DONE_COMPLETE;
 	host->caps2 = MMC_CAP2_NO_SDIO | MMC_CAP2_NO_MMC;
 	// host->max_blk_count = 1;
 
